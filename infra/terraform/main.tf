e1827fefb8d27ad9d4d8eecc36f9e9e5d22dcfaf
larissa-collossi <larissacollossi@gmail.com>
2025-09-03 01:13:09 +0000

    fix: update ami

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -2,6 +2,21 @@ provider "aws" {
   region = var.region
 }
 
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
 resource "aws_security_group" "allow_http" {
   name        = "allow_http"
   description = "Allow HTTP traffic"
@@ -22,7 +37,7 @@ resource "aws_security_group" "allow_http" {
 }
 
 resource "aws_instance" "fitness_app" {
-  ami           = var.ami_id
+  ami           = data.aws_ami.amazon_linux.id
   instance_type = var.instance_type
   security_groups = [aws_security_group.allow_http.name]
 
@@ -32,5 +47,3 @@ resource "aws_instance" "fitness_app" {
 }
 
 output "instance_public_ip" {
-  value = aws_instance.fitness_app.public_ip
-}
